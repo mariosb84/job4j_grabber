6e7c7e04dbbb138528e02a1188e7e6a37c245c8f
Sergei Baranov <mariosb84@mail.ru>
2022-01-06 13:06:38 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -9,17 +9,21 @@ import ru.job4j.utils.SqlRuDateTimeParser;
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            Element parent = td.parent();
-            String parseString = parent.child(5).text();
-            System.out.println(parseString);
-            System.out.println("Java_LocalDateTime: "
-                    + sqlRuDateTimeParser.parse(parseString));
+        for (int i = 1; i <= 5; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers" + "/" + i).get();
+            String page = doc.select("form > input[type=hidden]:nth-child(2)").attr("value");
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                Element parent = td.parent();
+                String parseString = parent.child(5).text();
+                System.out.println(parseString);
+                System.out.println("Java_LocalDateTime: "
+                        + sqlRuDateTimeParser.parse(parseString));
+                System.out.println("Page:" + page);
+            }
         }
     }
 }
