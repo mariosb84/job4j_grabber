ce6ef383a4029dfc2fce6d779efa7ff113f244f7
Sergei Baranov <mariosb84@mail.ru>
2022-01-16 21:36:07 +0300

    3. Архитектура проекта - Аргегатор Java Вакансий [#260359 #251527]

--- src/main/java/ru/job4j/Grab.java
+++ src/main/java/ru/job4j/Grab.java
@@ -3,9 +3,6 @@ package ru.job4j;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 
-import java.io.IOException;
-import java.text.ParseException;
-
 public interface Grab {
-    void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException, IOException, ParseException;
+    void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException;
 }
