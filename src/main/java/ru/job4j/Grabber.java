436299f1e4e0b220dbbba4d94168603d6e44af6c
Sergei Baranov <mariosb84@mail.ru>
2022-01-24 09:28:20 +0300

    7. Web. [#289478]

--- src/main/java/ru/job4j/Grabber.java
+++ src/main/java/ru/job4j/Grabber.java
@@ -7,6 +7,9 @@ import ru.job4j.utils.DateTimeParser;
 import ru.job4j.utils.SqlRuDateTimeParser;
 
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.Properties;
@@ -75,6 +78,26 @@ public class Grabber implements Grab {
         }
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes(Charset.forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 
     public static void main(String[] args) throws Exception {
         Grabber grab = new Grabber();
@@ -83,5 +106,6 @@ public class Grabber implements Grab {
         Store store = grab.store();
         Parse parse = grab.parse();
         grab.init(parse, store, scheduler);
+        grab.web(store);
     }
 }
